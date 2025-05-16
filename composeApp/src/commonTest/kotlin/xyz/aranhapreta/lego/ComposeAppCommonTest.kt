d49dc49fb2bf582f615a7aeb8f621dfbe10d25f8
Joao Sousa <joaocsousa@users.noreply.github.com>
2025-05-16 23:15:12 +0100

    Added working compose multiplatform android/iosApp; Added kover plugin.

---
+++ composeApp/src/commonTest/kotlin/xyz/aranhapreta/lego/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package xyz.aranhapreta.lego
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
